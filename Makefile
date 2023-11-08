b73c9e3cbfe8109db390e82814ab2ca69e0ad78f
Kode-Kun <luisantcard@gmail.com>
2023-11-08 12:40:29 -0500

    added elements and indices. Also fixed the Makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC = g++
 CFLAGS = -g -std=c++11 -Wall -lGL -lGLU -lglut -lGLEW -lglfw -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
 TARGET = ep1
-INC = /home/kode/src/OpenGL/incl
+INC = ./incl
 
-$(TARGET): ~/src/OpenGL/src/$(TARGET).cpp
-	$(CC) -I$(INC) ~/src/OpenGL/src/$(TARGET).cpp ~/src/OpenGL/src/glad.c -o ~/src/OpenGL/bin/$(TARGET) $(CFLAGS)
-	cp ~/src/OpenGL/bin/$(TARGET) ~/bin/$(TARGET)
+$(TARGET): ./src/$(TARGET).cpp
+	$(CC) -I$(INC) ./src/$(TARGET).cpp ./src/glad.c -o ./bin/$(TARGET) $(CFLAGS)
+	cp ./bin/$(TARGET) ~/bin/$(TARGET)
--- src/ep1.cpp
+++ src/ep1.cpp
@@ -54,15 +54,23 @@ int main(){
     glBindVertexArray(VAO);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left
-         0.5f, -0.5f, 0.0f, // right
-         0.0f,  0.5f, 0.0f  // top
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
     };
 
-    unsigned int VBO;
+    unsigned int VBO, EBO;
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // binding our vertex shader to OpenGL and compiling it
     unsigned int vertexShader;
@@ -107,7 +115,8 @@ int main(){
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
