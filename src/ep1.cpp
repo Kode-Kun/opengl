85882598b78b56317c3adfb990326a229b69968d
Kode-Kun <luisantcard@gmail.com>
2024-01-02 23:35:29 -0500

    version 0.0.1a

--- src/ep1.cpp
+++ src/ep1.cpp
@@ -96,6 +96,12 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if(glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS){
+       glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+    if(glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS){
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
